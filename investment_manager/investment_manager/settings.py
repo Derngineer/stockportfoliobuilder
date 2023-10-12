269a8d2c05f03003815cebcfcfb7ca842dd2410d
Derby <dmatderby@gmail.com>
2023-10-12 19:11:30 +0400

    addition of static files

--- investment_manager/investment_manager/settings.py
+++ investment_manager/investment_manager/settings.py
@@ -83,7 +83,12 @@ WSGI_APPLICATION = "investment_manager.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {}
+DATABASES = {
+    #     "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
+}
 
 
 # Password validation
@@ -129,6 +134,7 @@ STATIC_URL = "static/"
 STATICFILE_DIRS = (
     os.path.join(BASE_DIR,'static'),
     )
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
