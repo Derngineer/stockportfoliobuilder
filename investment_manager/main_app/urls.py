aaaec94a782d79cef739ca2c4122bd062b132361
Derby <dmatderby@gmail.com>
2023-10-12 18:25:11 +0400

    adding url recorrection

--- investment_manager/main_app/urls.py
+++ investment_manager/main_app/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 
 urlpatterns = [
+     path('', views.login_view, name='login'),
     path("settings/", views.settings, name="settings"),
     path("portfolio_builder/", views.portfolio_building, name ="building"),
     path("portfolio_tester/", views.portfolio_testing, name="testing"),
-    path('register/', views.register, name='register'),
-    path('login/', views.login_view, name='login'),
+    path('register/', views.register, name='register'),   
     path('logout/', views.user_logout, name='logout'),
     path('django_plotly_dash/', include('django_plotly_dash.urls')),
 ]
