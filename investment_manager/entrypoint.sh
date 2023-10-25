ba7a71dd18ff1e1972aae498653b4fba703e874f
Derby <dmatderby@gmail.com>
2023-10-25 21:32:59 +0400

    recalib

--- investment_manager/entrypoint.sh
+++ investment_manager/entrypoint.sh
@@ -5,4 +5,4 @@ python manage.py collectstatic --no-input
 
 DJANGO_SUPERUSER_PASSWORD=$SUPER_USER_PASSWORD python manage.py createsuperuser --username $SUPER_USER_NAME --email $SUPER_USER_EMAIL --noinput
 
-gunicorn investment_manager.wsgi:application --bind 0.0.0.0:8000
+gunicorn investment_manager.wsgi:application --bind 0.0.0.0:$PORT
